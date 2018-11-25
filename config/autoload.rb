2cfd0b3da9776c31c789b74d46752301c1188f7e
Helen <alenka352331@gamil.com>
2018-11-26 01:19:38 +0200

    autoloading

---
+++ config/autoload.rb
@@ -0,0 +1,7 @@
+require_relative '../entities/errors_message.rb'
+require_relative '../entities/validator.rb'
+require_relative '../entities/author.rb'
+require_relative '../entities/reader.rb'
+require_relative '../entities/book.rb'
+require_relative '../entities/order.rb'
+require_relative '../entities/library.rb'
