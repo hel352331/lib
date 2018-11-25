8465c4f03b7d7c808a0ed59db4b2214ef91f5be3
Helen <alenka352331@gamil.com>
2018-11-26 01:05:42 +0200

    validator

---
+++ entities/validator.rb
@@ -0,0 +1,11 @@
+module Validator
+  include ValidatorErrors
+
+  def check_class(entity, klass)
+    raise UndefinedClassError unless entity.is_a?(klass)
+  end
+
+  def check_empty(entity)
+    raise EmptyStringError if entity.empty?
+  end
+end
