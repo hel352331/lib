87b5070df5ab971a50ed058c58422688111c32f4
Helen <alenka352331@gamil.com>
2018-11-26 02:19:37 +0200

    adding pry

--- index.rb
+++ index.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative './config/autoload.rb'
 lib = Library.new
 author1 = Author.new(Faker::Book.author)
@@ -13,27 +15,27 @@ book5 = Book.new(Faker::Book.title, author5)
 reader1 = Reader.new(
   name: Faker::Name.first_name, email: Faker::Internet.email,
   city: Faker::Address.city, street: Faker::Address.street_name,
-  house: Faker::Number.between(1, 60)
+  house: Faker::Number.between(1, 7)
 )
 reader2 = Reader.new(
   name: Faker::Name.first_name, email: Faker::Internet.email,
   city: Faker::Address.city, street: Faker::Address.street_name,
-  house: Faker::Number.between(1, 60)
+  house: Faker::Number.between(1, 7)
 )
 reader3 = Reader.new(
   name: Faker::Name.first_name, email: Faker::Internet.email,
   city: Faker::Address.city, street: Faker::Address.street_name,
-  house: Faker::Number.between(1, 60)
+  house: Faker::Number.between(1, 7)
 )
 reader4 = Reader.new(
   name: Faker::Name.first_name, email: Faker::Internet.email,
   city: Faker::Address.city, street: Faker::Address.street_name,
-  house: Faker::Number.between(1, 10)
+  house: Faker::Number.between(1, 7)
 )
 reader5 = Reader.new(
   name: Faker::Name.first_name, email: Faker::Internet.email,
   city: Faker::Address.city, street: Faker::Address.street_name,
-  house: Faker::Number.between(1, 10)
+  house: Faker::Number.between(1, 7)
 )
 order1 = Order.new(reader5, book2)
 order2 = Order.new(reader3, book1)
@@ -41,9 +43,10 @@ order3 = Order.new(reader1, book2)
 order4 = Order.new(reader2, book2)
 order5 = Order.new(reader4, book2)
 lib.add(
-  author1, author2, author3, author4, author5,  book1, book2, book3, book4, book5,
+  author1, author2, author3, author4, author5, book1, book2, book3, book4, book5,
   reader1, reader2, reader3, reader4, reader5, order1, order2, order3, order4,
   order5
 )
 puts order1
 puts reader5
+binding.pry
